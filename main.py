f97cb591ca47fa6cd3f9ba117a843bc7f4ed689a
Jeff Blue <517262600@qq.com>
2022-04-25 23:14:43 +0100

    Add excel output

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import os
 import time
 from apis import *
 
-VALUABLE_THRESHOLD = 5000
+import pandas as pd
+
+VALUABLE_THRESHOLD = 10000
+
+DF_COLUMNS = ['ItemName', 'LocalPrice', 'TargetPrice', "ExpectRevenue", 'BuyAt', 'SellAt', "LastUpdate"]
 
 
 class FFXIVDealer:
@@ -25,6 +29,7 @@ class FFXIVDealer:
         url = MARKET_BOARD_LISTING_URL.format(world_id, item_id)
         resp = self.s.get(url, timeout=5)
         res_json = resp.json()
+        # save fields
         listings = res_json['listings']
         world_name = res_json['worldName']
         min_price_nq = res_json['minPriceNQ']
@@ -35,14 +40,17 @@ class FFXIVDealer:
         average_price_nq = res_json['averagePriceNQ']
         prices = [i['pricePerUnit'] for i in listings]
         regular_sale_velocity = res_json['regularSaleVelocity']
+
         # get chinese name for item
         if not self.item_info.get(item_id):
             resp = self.s.get(ITEM_INFO_URL.format(item_id), timeout=5)
             item_info_res_json = resp.json()
             self.item_info[item_id] = {}
             self.item_info[item_id]['item_name'] = item_info_res_json['Singular_chs']
+            # update cache json
             with open('./item.json', 'w') as f:
                 json.dump(self.item_info, f)
+        item_name = self.item_info[item_id]['item_name']
 
         return {'prices': prices,
                 "world_name": world_name,
@@ -52,13 +60,13 @@ class FFXIVDealer:
                 'max_price_hq': max_price_hq,
                 'average_price_hq': average_price_hq,
                 'average_price_nq': average_price_nq,
-                'item_name': self.item_info[item_id]['item_name'],
+                'item_name': item_name,
                 'regular_sale_velocity': regular_sale_velocity
                 }
 
     def get_market_history(self, world_id: str, item_id: str) -> dict:
         """
-        api return json like below
+        the universalis api return json like below
         {
           "itemID": 34691,
           "worldID": 1171,
@@ -97,18 +105,20 @@ class FFXIVDealer:
         # item_list = self.s.get(MARKET_ALL_ITEMS_ID_URL, timeout=5).json()[::-1]
         # get fixed items only
         item_list = TARGET_ITEMS
+        df = pd.DataFrame(columns=DF_COLUMNS)
         # loop for target selling items
         for item_id in item_list:
             # get local server price
             local_sever_item_info = self.get_board_list_info(LOCAL_SERVER[0], item_id)
             local_server_histories = self.get_market_history(LOCAL_SERVER[0], item_id)
-            local_price = local_sever_item_info['average_price_hq']
+            local_price_hq = local_sever_item_info['min_price_hq']
+            local_price_nq = local_sever_item_info['min_price_nq']
             local_server_name = local_sever_item_info['world_name']
             local_sale_velocity = local_server_histories['sale_velocity']
             # target server prices
             items = []
-            average_prices_nq = []
-            average_prices_hq = []
+            lowest_price_nq = []
+            lowest_price_hq = []
             server_names = []
             # TARGET_SERVERS is a 2d array
             for server_list in TARGET_SERVERS:
@@ -118,25 +128,43 @@ class FFXIVDealer:
                     item_last_update = item_history.get("last_upload_time").strftime('%Y-%m-%d %H:%m:%S')
                     item_info['item_last_update'] = item_last_update
                     items.append(item_info)
-                    average_prices_nq.append(item_info['average_price_nq'])
-                    average_prices_hq.append(item_info['average_price_hq'])
+                    lowest_price_hq.append(item_info['min_price_hq'])
+                    lowest_price_nq.append(item_info['min_price_nq'])
                     server_names.append(item_info['world_name'])
             # analysis & report
-            idx_hq = np.argmin([i['average_price_hq'] for i in items])
-            gap_hq = local_price - items[idx_hq]['average_price_hq']
-            idx_nq = np.argmin([i['average_price_nq'] for i in items])
-            gap_nq = local_price - items[idx_nq]['average_price_nq']
+            idx_hq = np.argmin(lowest_price_hq)
+            idx_nq = np.argmin(lowest_price_nq)
+            target_price_hq = items[idx_hq]['min_price_hq']
+            target_price_nq = items[idx_nq]['min_price_nq']
+            gap_hq = local_price_hq - target_price_hq
+            gap_nq = local_price_nq - target_price_nq
             if gap_hq > VALUABLE_THRESHOLD:
-                print(f"{self.item_info[item_id]['item_name']}HQ, Buy at {server_names[idx_hq]}, Sell at {local_server_name},"
+                print(f"{self.item_info[item_id]['item_name']}★, Buy at {server_names[idx_hq]},"
+                      f" Sell at {local_server_name},"
                       f" Expect revenue: {gap_hq}, Sale velocity: {local_sale_velocity}, "
                       f"last update: {items[idx_hq]['item_last_update']}")
+                # DF_COLUMNS = ['ItemName', 'LocalPrice', 'TargetPrice', 'BuyAt', 'SellAt', "LastUpdate"]
+                item_df = pd.DataFrame([[f"{self.item_info[item_id]['item_name']}★",
+                                         local_price_hq, target_price_hq, gap_hq,
+                                        server_names[idx_hq], local_server_name,
+                                        items[idx_hq]['item_last_update']]], columns=DF_COLUMNS)
+                df = pd.concat([df, item_df])
             if gap_nq > VALUABLE_THRESHOLD:
-                print(f"{self.item_info[item_id]['item_name']}NQ, Buy at {server_names[idx_nq]}, Sell at {local_server_name},"
+                print(f"{self.item_info[item_id]['item_name']}, Buy at {server_names[idx_nq]}, Sell at {local_server_name},"
                       f" Expect revenue: {gap_nq}, Sale velocity: {local_sale_velocity}, "
                       f"last update: {items[idx_nq]['item_last_update']}")
+                item_df = pd.DataFrame([[f"{self.item_info[item_id]['item_name']}★",
+                                         local_price_nq, target_price_nq, gap_nq,
+                                        server_names[idx_nq], local_server_name,
+                                        items[idx_hq]['item_last_update']]], columns=DF_COLUMNS)
+                df = pd.concat([df, item_df])
             else:
                 print(f"{self.item_info[item_id]['item_name']}, not valuable")
-            time.sleep(0.3)
+        # save result to excel
+        cur_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        if not os.path.exists('./excel_results/'):
+            os.mkdir('./excel_results/')
+        df.to_excel(f"./excel_results/{cur_time}.xlsx", index=False)
 
 
 if __name__ == '__main__':
--- user_settings.py
+++ user_settings.py
@@ -1,19 +1,23 @@
+# Configure your own setting in below lines
 # 写入你的服务器编号，下面是示例
+# Write your own server name below
 LOCAL_SERVER = ['ShenQuanHen']
+# 想要倒卖的物品号, 可由universalis.app的物品页面获得
+# for example https://universalis.app/market/34535 means the item id is 34535
+TARGET_ITEMS = ['34691', '34541', '34391', '34241', '34091',
+                '33942', '33932', '35463']
 
+
+# Other macros below (**NO** Need To Modify)
 CN_MOGULI = ['BaiYinXiang', 'BaiJinHuanXiang', 'ShenQuanHen', 'ChaoFengTing', 'LvRenZhanQiao',
                   'FuXiaoZhiJian', 'LongChaoShenDian', 'MengYuBaoJing']
-
+CN_LUXINGNIAO = ['HongYuHai', 'YanXia', 'JingYuZhuangYuan']
 # 北美服务器 NA servers
 NA_AETHER = ['Adamantoise', 'Cactuar', 'Faerie', 'Gilgamesh', 'Jenova', 'Midgardsormr', 'Sargatanas', 'Siren']
 NA_PRIMAL = ['Behemoth', 'Excalibur', 'Exodus', 'Famfrit', 'Hyperion', 'Lamia', 'Leviathan', 'Ultros']
 NA_CRYSTAL = ['Balmung', 'Brynhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Malboro' , 'Mateus', 'Zalera']
 
-
-TARGET_ITEMS = ['34691', '34541', '34391', '34241', '34091',
-                '33942', '33932', '35463']
-
-SERVERS_COLLECTION = [CN_MOGULI, NA_AETHER, NA_PRIMAL, NA_CRYSTAL]
+SERVERS_COLLECTION = [CN_MOGULI, CN_LUXINGNIAO, NA_AETHER, NA_PRIMAL, NA_CRYSTAL]
 
 TARGET_SERVERS = []
 for server in LOCAL_SERVER:
